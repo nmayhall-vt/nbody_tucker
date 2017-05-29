0f4df52f16e9f873e654e3c8b0978ef1d9762699
nmayhall@vt.edu <nmayhall@sinatra.chem.vt.edu>
2017-05-29 19:16:28 -0400

    updated data

--- tests/grid_4x6/sub_pt.sh
+++ tests/grid_4x6/sub_pt.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+for nb in {0..3}; do
+	for i in $( ls j12.*.m ); do
+		echo Submitting: $i
+		../../nbtuck_davidson.py -nb $nb -ju ev -j $i -direct 0 -mit 1 -dmit 40 -nr 1 -pt 2 > ${i::-2}.nb$nb.pt2.out  & 
+		../../nbtuck_davidson.py -nb $nb -ju ev -j $i -direct 0 -mit 1 -dmit 40 -nr 1 -pt 2 -pt_type en > ${i::-2}.nb$nb.pt2.en.out  
+	done
+done
+
 for nb in {4..4}; do
 	for i in $( ls j12.*.m ); do
 		echo Submitting: $i
 		../../nbtuck_davidson.py -nb $nb -ju ev -j $i -direct 1 -mit 1 -dmit 40 -nr 1 -pt 2 > ${i::-2}.nb$nb.pt2.out  
+		../../nbtuck_davidson.py -nb $nb -ju ev -j $i -direct 1 -mit 1 -dmit 40 -nr 1 -pt 2 -pt_type en > ${i::-2}.nb$nb.pt2.en.out  
 	done
 done
