efcce378530c6070f805ce1789cf0fa6410ca07b
nmayhall@vt.edu <nmayhall@sinatra.chem.vt.edu>
2018-03-07 18:15:22 -0500

    added s2 and sz. oddly enough, the ground state has s2 being 3-2*sqrt(2)

--- mera_binary_4site.py
+++ mera_binary_4site.py
@@ -57,8 +57,9 @@ chi = 2
     
 H, tmp, S2, Sz = form_hdvv_H(lattice,j12)
 H.shape = (2,2,2,2,2,2,2,2)
+S2.shape = (2,2,2,2,2,2,2,2)
+Sz.shape = (2,2,2,2,2,2,2,2)
 w1 = scipy.linalg.orth(np.random.rand(4,4))[:,0:chi]
-w1 = w1.T
 w2 = w1
 w3 = scipy.linalg.orth(np.random.rand(chi*chi,chi*chi))[:,0:chi]
 
@@ -68,12 +69,21 @@ if chi == 2:
     w2 = w1
     w3 = w1
 
+if chi == 1:
+    w1 = np.array([0,1,-1,0])/np.sqrt(2)
+    w1 = w1.T
+    w2 = w1
+
 w1.shape = (2,2,chi)
 w2.shape = (2,2,chi)
 w3.shape = (chi,chi,chi)
 
+#q1.shape = (2,2,4-chi)
+#q2.shape = (2,2,4-chi)
+#q3.shape = (4-chi,4-chi,4-chi)
+
 U = np.eye(4)
-U += .01*np.random.rand(4,4)
+U += .1*np.random.rand(4,4)
 U,s,V = np.linalg.svd(U)
 U = U.dot(V)
 U.shape = (2,2,2,2)
@@ -90,9 +100,8 @@ w1_errv = []
 w2_errv = []
 w3_errv = []
 
-mera = "jkmn,imo,nlp,opq->ijklq" # with u, w1, w2, w3
-for it in range(1000):
-    m = np.einsum(mera, U, w1, w2, w3)
+for it in range(10000):
+    m = np.einsum("jkmn,imo,nlp,opq->ijklq", U, w1, w2, w3)
     H_curr = np.einsum("ijklq,ijklmnop,mnopr->qr",m,H,m)
     l_curr, v_curr = np.linalg.eigh(H_curr)
     v = v_curr[:,0]
@@ -100,53 +109,64 @@ for it in range(1000):
     sigma = np.einsum("ijklmnop,mnopq,q->ijkl",H,m,v)
     
     E_curr = np.einsum('i,jki,lmj,nok,pqmn,lpqo->',v,w3,w1,w2,U,sigma)
+    S2_curr = np.einsum("q,ijklq,ijklmnop,mnopr,r->",v,m,S2,m,v)
+    Sz_curr = np.einsum("q,ijklq,ijklmnop,mnopr,r->",v,m,Sz,m,v)
     #E_curr = np.einsum("ijkl,jkmn,imo,nlp,opq,q->",sigma,U,w1,w2,w3,v)
-
-    if abs(E_curr-E_prev)<1e-8 and it > 2 :
+    
+    #mera_state = np.einsum('i,jki,lmj,nok,pqmn->lpqo',v,w3,w1,w2,U)
+    #normsq = np.einsum('ijkl,ijkl->',mera_state,mera_state)
+    #print " Norm: %12.8f " % normsq
+    
+    if abs(E_curr-E_prev)<1e-16 and it > 2 :
         pass
         break
     
+    U_old = U
     E_prev = E_curr
-    for it2 in range(10):
+    for it2 in range(1):
         Gu_curr = np.einsum('i,jki,lmj,nok,lpqo->pqmn',v,w3,w1,w2,sigma)
-       
-        U_old = U
+      
+        #tmp1 = np.einsum('i,jki,lmj,nok,pqmn,lpqorstu,stvw,rvx,wuy,xyz,z->',v,w3,w1,w2,U,H,U,w1,w2,w3,v)
         Gu_curr = Gu_curr.reshape(4,4)
         u0,s0,v0 = np.linalg.svd(Gu_curr)
-        U_new = -v0.T.dot(u0.T)
+        U_new = -u0.dot(v0)
         
         U_new.shape = (2,2,2,2)
         U = U_new 
-
-    w1_old = w1
-    w2_old = w2
-    w3_old = w3
-    for it2 in range(10):
-        G1_curr = np.einsum('i,jki,nok,pqmn,lpqo->lmj',v,w3,w2,U,sigma)
-        G2_curr = np.einsum('i,jki,lmj,pqmn,lpqo->nok',v,w3,w1,U,sigma)
-      
-        G1_curr = G1_curr.reshape(4,chi)
-        u1,s1,v1 = np.linalg.svd(G1_curr)
-        u1 = u1[:,0:chi]
-        v1 = v1[0:chi,:]
-        w1 = -u1.dot(v1)
-        w1.shape = (2,2,chi)
         
-        G2_curr = G2_curr.reshape(4,chi)
-        u2,s2,v2 = np.linalg.svd(G2_curr)
-        u2 = u2[:,0:chi]
-        v2 = v2[0:chi,:]
-        w2 = -u2.dot(v2)
-        w2.shape = (2,2,chi)
+        #tmp2 = np.einsum('i,jki,lmj,nok,pqmn,lpqorstu,stvw,rvx,wuy,xyz,z->',v,w3,w1,w2,U,H,U,w1,w2,w3,v)
+        #print "E: %12.8f %12.8f" %(tmp1,tmp2)
 
-        for it3 in range(1):
-            G3_curr = np.einsum('i,lmj,nok,pqmn,lpqo->jki',v,w1,w2,U,sigma)
-            G3_curr = G3_curr.reshape(chi*chi,chi)
-            u3,s3,v3 = np.linalg.svd(G3_curr)
-            u3 = u3[:,0:chi]
-            v3 = v3[0:chi,:]
-            w3 = -u3.dot(v3)
-            w3.shape = (chi,chi,chi)
+        w1_old = w1
+        w2_old = w2
+        w3_old = w3
+
+        for it2 in range(1):
+            G1_curr = np.einsum('i,jki,nok,pqmn,lpqo->lmj',v,w3,w2,U,sigma)
+            G2_curr = np.einsum('i,jki,lmj,pqmn,lpqo->nok',v,w3,w1,U,sigma)
+          
+            G1_curr = G1_curr.reshape(4,chi)
+            u1,s1,v1 = np.linalg.svd(G1_curr)
+            u1 = u1[:,0:chi]
+            v1 = v1[0:chi,:]
+            w1 = -u1.dot(v1)
+            w1.shape = (2,2,chi)
+            
+            G2_curr = G2_curr.reshape(4,chi)
+            u2,s2,v2 = np.linalg.svd(G2_curr)
+            u2 = u2[:,0:chi]
+            v2 = v2[0:chi,:]
+            w2 = -u2.dot(v2)
+            w2.shape = (2,2,chi)
+    
+            for it3 in range(1):
+                G3_curr = np.einsum('i,lmj,nok,pqmn,lpqo->jki',v,w1,w2,U,sigma)
+                G3_curr = G3_curr.reshape(chi*chi,chi)
+                u3,s3,v3 = np.linalg.svd(G3_curr)
+                u3 = u3[:,0:chi]
+                v3 = v3[0:chi,:]
+                w3 = -u3.dot(v3)
+                w3.shape = (chi,chi,chi)
    
 
     diis = 0
@@ -154,7 +174,7 @@ for it in range(1000):
         # U error vector
         e_u = U - U_old 
         e_u.shape = (16,1)
-           
+       
         # w1 error vector
         p = cp.deepcopy(w1_old)
         d = cp.deepcopy(G1_curr)
@@ -164,7 +184,7 @@ for it in range(1000):
         d = d.dot(d.T)
         e_w1 = p.dot(d) - d.dot(p)
         e_w1.shape = (16,1)
- 
+    
         # w2 error vector
         p = cp.deepcopy(w2_old)
         d = cp.deepcopy(G2_curr)
@@ -197,7 +217,7 @@ for it in range(1000):
             w3_errv = np.hstack((w1_errv,e_w3))
  
         U0_diis.append(Gu_curr) 
-        if it>0:
+        if it>20:
             errv = cp.deepcopy(U0_errv)
             n_evecs = errv.shape[1]
             d = Gu_curr
@@ -220,12 +240,12 @@ for it in range(1000):
                     d += x[i]*envs[i]
                     extrap_err_vec += x[i]*errv[:,i]
                 
-                print " DIIS Coeffs"
-                for i in x:
-                    print "  %12.8f" %i
+                #print " DIIS Coeffs"
+                #for i in x:
+                #    print "  %12.8f" %i
             
             u0,s0,v0 = np.linalg.svd(d)
-            U_new = -v0.T.dot(u0.T)
+            U_new = -u0.dot(v0)
             U_new.shape = (2,2,2,2)
             #U = U_new 
         print " Norms: %8.3e %8.3e %8.3e %8.3e"%(np.linalg.norm(e_u),np.linalg.norm(e_w1),np.linalg.norm(e_w2),np.linalg.norm(e_w3)),
@@ -235,6 +255,6 @@ for it in range(1000):
     w2_prev = w2
     w3_prev = w3
 
-    print " %5i E_curr: %12.8f " %(it, E_curr)
+    print " %5i E_curr: %12.8f  S2_curr: %12.8f  Sz_curr: %12.8f " %(it, E_curr, S2_curr, Sz_curr)
     
 
