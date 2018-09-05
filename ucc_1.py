325669f6a9d666d03cf29f42e781c39af6444c7f
nmayhall-vt <nmayhall@vt.edu>
2018-09-04 20:28:43 -0400

    changed

--- ucc_1.py
+++ ucc_1.py
@@ -3,6 +3,7 @@
 #import autograd.numpy as np
 #from autograd import grad  
 #import numpy as np
+import sys
 import scipy
 import scipy.linalg
 import scipy.io
@@ -88,17 +89,18 @@ def callback(inp):
     global niter
     print(" Iter: %5i  Energy: %16.12f Norm of gradient %12.1e"%(niter, curr_energy, curr_grad_norm))
     niter += 1
+    sys.stdout.flush()
     return
 
 def form_energy(inp):
     #U0 = form_hdvv_U_1v(lattice,inp)
-    global U0
+    global U0,v0
     U0 = np.zeros(H.shape)
     for ui in range(len(operators)):
         U0 += inp[ui] * operators[ui]
     Uv = scipy.sparse.linalg.expm_multiply(1j*U0,v0)
 
-    E = np.vdot(Uv,H.dot(Uv)) 
+    E = np.vdot(Uv,H.dot(Uv)) / np.vdot(Uv,Uv)
     global curr_energy 
     curr_energy += np.vdot(Uv,H.dot(Uv)) 
     if np.isclose(E.imag, 0):
@@ -112,26 +114,27 @@ def form_energy(inp):
 
 def form_gradient_param(pi):
    
-    step_size = 1e-7
-    global U0
+    step_size = 1e-6
+    global U0,v0
     Ucur = U0 + operators[pi]*step_size
     Uv = scipy.sparse.linalg.expm_multiply(1j*Ucur,v0)
 
-    ef = np.vdot(Uv,H.dot(Uv))  
+    ef = np.vdot(Uv,H.dot(Uv)) / np.vdot(Uv,Uv)  
     if np.isclose(ef.imag, 0):
         ef = ef.real
     
     Ucur = U0 - operators[pi]*step_size
     Uv = scipy.sparse.linalg.expm_multiply(1j*Ucur,v0)
 
-    er = np.vdot(Uv,H.dot(Uv))  
+    er = np.vdot(Uv,H.dot(Uv)) / np.vdot(Uv,Uv)  
     if np.isclose(er.imag, 0):
         er = er.real
     return np.real(ef - er) / (2*step_size)
 
 def form_gradient(inp):
    
-    pool = Pool() 
+    """ 
+    pool = Pool(1) 
     results = pool.map(form_gradient_param, range(len(inp)))
     pool.close() 
     pool.join() 
@@ -140,7 +143,6 @@ def form_gradient(inp):
     der = np.zeros((len(inp)))
     for pi in range(len(inp)):
         der[pi] = form_gradient_param(pi)
-    """ 
     global curr_grad_norm
     curr_grad_norm = np.linalg.norm(der)
     return der 
@@ -173,18 +175,23 @@ input_data = [a,b]
 prev_E = 0
 for it in range(100):
 
-    if it>0:
-        a = (np.random.rand(n_sites*(n_sites-1)/2)-.5)/100
-        U = form_hdvv_U_1v(lattice,a)
-        #U = scipy.sparse.linalg.expm(1j*U)
-        #v0 = U.dot(v0)
-        v0 = scipy.sparse.linalg.expm_multiply(1j*U,v0)
+    sys.stdout.flush()
 
+    if it>0:
+        U0 = np.zeros(H.shape)
+        global U0,v0
+        for ui in range(len(operators)):
+            U0 += a[ui] * operators[ui]
+        v0 = scipy.sparse.linalg.expm_multiply(1j*U0,v0)
+        a = (np.random.rand(n_sites*(n_sites-1)/2)-.5)/100
+        print(" Initial energy of new iterate: %16.12f" %form_energy(a))
     #result = minimize(form_energy, a, method='BFGS', options={'gtol': 1e-10, 'disp': True}, callback=callback)
+    #result = minimize(form_energy, a, method='L-BFGS-B', jac=form_gradient, options={'ftol': 1e-12,'gtol': 1e-6, 'disp': True}, callback=callback)
     result = minimize(form_energy, a, method='BFGS', jac=form_gradient, options={'gtol': 1e-8, 'disp': True}, callback=callback)
     #result = minimize(form_energy, a, method='L-BFGS-B', jac=grad_E, options={'gtol': 1e-2, 'disp': True})
     
     print " Iteration: %4i  Current Energy: %16.12f  Delta Energy: %16.12f" %(it, result.fun, result.fun-prev_E)
+    a = cp.deepcopy(result.x)
     print(result.x)
     if abs(result.fun - prev_E) < 1e-10:
         break
